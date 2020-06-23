6df08767f49cea80a09fd8e7aeb15dc0ddbd5000
William Grundler <wrgrundler@gmail.com>
2020-06-23 16:57:21 -0700

    created axios request in API util

---
+++ src/utils/API.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default{
+    search: function() {
+        return axios.get('https://randomuser.me/api/?results=100&nat=US&inc=name,phone,dob,email,picture')
+    }
+}
+
